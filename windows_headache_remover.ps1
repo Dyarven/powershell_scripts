4646efbdfb79e504e696f4594882da935afd0733
David <81330265+Dyarven@users.noreply.github.com>
2023-02-27 23:18:00 +0100

    Add files via upload

---
+++ windows_headache_remover.ps1
@@ -0,0 +1,23 @@
+#makes windows less of a pain after a clean install
+
+#bye bye my dear cortana 
+Get-AppxPackage -Name Microsoft.549981C3F5F10 | Remove-AppxPackage
+
+#why would i want the weather forecast, i have a phone
+Get-AppxPackage -Name Microsoft.BingWeather | Remove-AppxPackage -ErrorAction Stop
+
+#disables xbox game bar
+Get-AppxPackage -Name Microsoft.XboxGamingOverlay | Remove-AppxPackage -ErrorAction Stop
+
+#disables uac
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableLUA -Value 0
+
+#show file extensions
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Value 0
+
+#dark theme for my eyes
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type DWord
+
+#godmode shortcut on desktop
+$GodModePath = "$env:USERPROFILE\Desktop\GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
+New-Item -ItemType Directory -Force -Path $GodModePath
