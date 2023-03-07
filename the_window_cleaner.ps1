2d7a88079e61d9bcbeb901d5d2b17793a8ace01d
David <81330265+Dyarven@users.noreply.github.com>
2023-03-07 07:09:57 +0100

    Update the_window_cleaner.ps1

--- the_window_cleaner.ps1
+++ the_window_cleaner.ps1
@@ -22,3 +22,17 @@ New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Pe
 #godmode shortcut on taskbar
 $GodModePath = "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
 New-Item -ItemType Directory -Force -Path $GodModePath
+
+#update powershell
+$latestVer = Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/latest" -UseBasicParsing
+$downloadLink = $latestVer.Links | Where-Object { $_.href -like '*powershell-*-win-x64.msi' } | Select-Object -First 1 -ExpandProperty href
+Invoke-WebRequest -Uri $downloadLink -OutFile "$env:USERPROFILE\Downloads\PowerShell-latest-win-x64.msi"
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i ""$env:USERPROFILE\Downloads\PowerShell-latest-win-x64.msi"" /quiet /norestart" -Wait
+$PowerShellPath = "C:\Program Files\PowerShell\pwsh.exe"
+$WindowsPowerShellPath = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
+$PathValue = [Environment]::GetEnvironmentVariable("Path", "Machine")
+$PathValue = $PathValue -replace [regex]::Escape($WindowsPowerShellPath), $PowerShellPath
+[Environment]::SetEnvironmentVariable("Path", $PathValue, "Machine")
+
+#refresh env vars
+Stop-Process -Name explorer
