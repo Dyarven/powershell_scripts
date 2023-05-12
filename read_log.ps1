a075f887a4f305595f841d089f701bc5460a51aa
David <81330265+Dyarven@users.noreply.github.com>
2023-05-12 02:07:57 +0200

    Update read_log.ps1

--- read_log.ps1
+++ read_log.ps1
@@ -3,5 +3,5 @@ $logfile = "C:\folderx\foldery\file.log"
 if(Test-Path $logfile) {
     Get-Content $logfile -Wait
 } else {
-    Write-Host "O arquivo '$logFilePath' non existe."
+    Write-Host "O arquivo '$logfile' non existe."
 }
