d6555f2af9627bc5fb3f2c98ea6b43223e3facfd
David <81330265+Dyarven@users.noreply.github.com>
2023-05-14 05:13:19 +0200

    Update rotate_log.ps1

--- rotate_log.ps1
+++ rotate_log.ps1
@@ -1,13 +1,13 @@
-$logFile = "C:\program\logs\logname.log"
+$logFile = "C:\scripts\logs\logname.log"
 $cap = 2147483648 # 2GB
 
 if ((Get-Item $logFile).length -gt $cap) {
     $timestamp = Get-Date -Format "ddMMyyyyHHmmss"
-    $rotatedLog = "C:\program\logs\rotated_logname_$timestamp.log"
+    $rotatedLog = "C:\scripts\logs\rotated_logname_$timestamp.log"
     Rename-Item $logFile $rotatedLog
     Start-Sleep -Seconds 5
     # if the program doesn't generate a new log file we do so
-    $logFile = Get-ChildItem -Path "C:\program\logs" -Filter "logname.log" -Recurse | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+    $logFile = Get-ChildItem -Path "C:\scripts\logs" -Filter "logname.log" -Recurse | Sort-Object LastWriteTime -Descending | Select-Object -First 1
     if ($logFile -ne $null) {
         $logFile = $logFile.FullName
     } else {
