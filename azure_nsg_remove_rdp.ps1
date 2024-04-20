a87fe0e972932bc39d77e817ac83e6fd0805ccb7
David <81330265+Dyarven@users.noreply.github.com>
2024-04-20 10:18:33 +0200

    Add files via upload

---
+++ azure_nsg_remove_rdp.ps1
@@ -0,0 +1,20 @@
+Connect-AzAccount
+
+# Get network security groups
+$nsgs = Get-AzNetworkSecurityGroup
+
+foreach ($nsg in $nsgs) {
+    $nsgRules = Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $nsg 
+
+    foreach ($nsgRule in $nsgRules) { 
+        if ($nsgRule.Name -like "*Allow-RDP*") {
+            Write-Output "Removing rule $($nsgRule.Name) from $($nsg.Name)"
+            Remove-AzNetworkSecurityRuleConfig -Name $nsgRule.Name -NetworkSecurityGroup $nsg -Force
+        } 
+    }
+
+    # Only set the NSG if rules were removed
+    if ($nsgRules -ne $null) {
+        $nsg | Set-AzNetworkSecurityGroup
+    }
+}
