ea786d6706aa90907bce0eb4b93eb2bbb063d8b2
David <81330265+Dyarven@users.noreply.github.com>
2024-04-20 10:19:36 +0200

    Update ad_clear_location_search.ps1

--- ad_clear_location_search.ps1
+++ ad_clear_location_search.ps1
@@ -1,18 +1,18 @@
-#This script looks for devices that meet the criteria of location and name and lists them to the user so he can delete their locations so they are not displayed on certain apps that work with LDAP.
+# this script looks for devices that meet the criteria of location and name and lists them to the user so he can delete their locations so they are not displayed on certain apps that work with LDAP.
 
 Import-Module ActiveDirectory
 
-#Define the Domain Controller
+# define the Domain Controller
 $dc = "Sample"
 
-#define where to start searching (Get-ADComputer defaults to OU=computers)
+# define where to start searching (Get-ADComputer defaults to OU=computers)
 $ou = "OU=Desktops,OU=Devices,DC=Sample,DC=whatever,DC=com"
 
 function UpdateDeviceInAD {
     $location = Read-Host "Enter the device location (2 to 5 digits): "
     $fixedLocation = '*' + $location.PadLeft(5, '0') + '*'
 
-    # Search for devices that match the location and have a name starting with D
+    # search for devices that match the location and have a name starting with D
     $devices = Get-ADComputer -Filter {Location -like $fixedLocation -and Name -like "D*"} -Properties Description, Location -Server $dc -SearchBase $ou -ErrorAction SilentlyContinue
 
     if (!$devices) {
@@ -20,7 +20,7 @@ function UpdateDeviceInAD {
         return
     }
 
-    # Displays the list of devices
+    # displays the list of devices
     Write-Host "Dispositivos encontrados:"
     $index = 1
     foreach ($device in $devices) {
@@ -31,11 +31,11 @@ function UpdateDeviceInAD {
         $index++
     }
 
-    # Ask the user which devices to delete
+    # ask the user which devices to delete
     $deviceNumbers = Read-Host "Introduce los dispositivos a los que quieres borrarles el Location, separados por comas (ej. 1,4,5): "
     $deviceNumbers = $deviceNumbers -split ','
 
-    # Delete the selected devices
+    # delete the selected devices
     foreach ($deviceNumber in $deviceNumbers) {
         $device = $devices[$deviceNumber - 1]
         Write-Host "Borrando Location en el dispositivo antiguo: '$($device.Name)'..."
